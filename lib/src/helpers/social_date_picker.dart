8e2ed1fc274d87a892af19153cb064ab783c3db0
Farzad <farzadi.fm@gmail.com>
2020-12-16 23:01:26 +0330

    Change date picker color

--- lib/src/helpers/social_date_picker.dart
+++ lib/src/helpers/social_date_picker.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:restfulness/constants.dart';
 import 'package:restfulness/src/blocs/social/social_provider.dart';
 
 class SocialDatePicker {
@@ -9,7 +10,8 @@ class SocialDatePicker {
     final socialBloc = SocialProvider.of(context);
 
     DatePicker.showDatePicker(context,
-
+        theme: DatePickerTheme(
+            doneStyle: TextStyle(color: primaryColor, fontSize: 16)),
         showTitleActions: true,
         minTime: DateTime(2020, 1, 1),
         maxTime: DateTime.now(), onChanged: (date) {
