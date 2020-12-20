5fdbe4740b7606b4231b00978bf452df525c9988
Farzad <farzadi.fm@gmail.com>
2020-12-20 14:37:31 +0330

    Fix demo

--- lib/src/screens/main_screen.dart
+++ lib/src/screens/main_screen.dart
@@ -83,6 +83,7 @@ class _MainScreenState extends State<MainScreen> {
           ),
           leading: Stack(
             children: [
+              if(isDemo)
               Positioned(
                 child: InkWell(
                   onTap: () => {
