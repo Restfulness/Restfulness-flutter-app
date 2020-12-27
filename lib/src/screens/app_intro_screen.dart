3712c9814ff566bde7095c1adc46dcb857b53824
Farzad <farzadi.fm@gmail.com>
2020-12-27 16:37:47 +0330

    Add intro to app

---
+++ lib/src/screens/app_intro_screen.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:restfulness/src/screens/login/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppIntroScreen extends StatefulWidget {
+  @override
+  _AppIntroScreenState createState() => _AppIntroScreenState();
+}
+
+class _AppIntroScreenState extends State<AppIntroScreen> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+
+    _saveIntro().then((value){
+      if(value){
+        _redirectToPage(context, LoginScreen());
+      }
+    });
+  }
+
+  Widget _buildImage(String assetName) {
+    return Align(
+      child: Image.asset('assets/intro/$assetName.png', width: 350.0),
+      alignment: Alignment.bottomCenter,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      pages: [
+        PageViewModel(
+          title: "Make it visible",
+          body:
+              "Manage your team's valuable links by centralizing them in a dedicated place, categorize them, and search through them easily.",
+          image: _buildImage('img1'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Make it trackable",
+          body:
+              "Follow exciting links that each team's members are finding and make these activities bold and smooth.",
+          image: _buildImage('img2'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Add your server",
+          body:
+              "On the login page by tapping on the gear icon you can configure your server address and by going to settings you can change it, or you can use the demo server.",
+          image: _buildImage('img3'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Control your links",
+          body: "You have two options to control your links, by going to settings you can turn on/off the preview",
+          image: Align(
+            child: Column(
+              children: [
+                SizedBox(height:200,),
+                Image.asset('assets/intro/animation1.gif', width: 250.0),
+              ],
+            ),
+            alignment: Alignment.center,
+          ),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Update categories",
+          body: "By Clicking on plus or hashtag you can update your category",
+          image: Align(
+            child: Column(
+              children: [
+                SizedBox(height: 150,),
+                Image.asset('assets/intro/img4.png', width: 250.0),
+                SizedBox(height: 25,),
+                Image.asset('assets/intro/img5.png', width: 250.0)
+              ],
+            ),
+            alignment: Alignment.center,
+          ),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+    );
+  }
+
+
+  Future<void> _redirectToPage(BuildContext context, Widget page) async {
+    final MaterialPageRoute<bool> newRoute =
+    MaterialPageRoute<bool>(builder: (BuildContext context) => page);
+
+    final bool nav = await Navigator.of(context)
+        .pushAndRemoveUntil<bool>(newRoute, ModalRoute.withName('/'));
+    if (nav == true) {
+      //TODO: init
+    }
+  }
+
+  Future<bool> _saveIntro() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'intro';
+    final isSaved = prefs.setBool(key, true);
+    return isSaved;
+  }
+
+}
--- lib/src/screens/decision_screen.dart
+++ lib/src/screens/decision_screen.dart
@@ -6,6 +6,7 @@ import 'package:restfulness/src/blocs/category/categories_provider.dart';
 import 'package:restfulness/src/blocs/link/links_provider.dart';
 import 'package:restfulness/src/blocs/social/social_provider.dart';
 import 'package:restfulness/src/resources/repository.dart';
+import 'package:restfulness/src/screens/app_intro_screen.dart';
 import 'package:restfulness/src/utils/json_utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -25,28 +26,34 @@ class _DecisionScreenState extends State<DecisionScreen> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
+      _readIntro().then((value) async {
+        if (!value) {
+          _redirectToPage(context, AppIntroScreen());
+        } else {
+          await repository.initializationAuth;
 
-      await repository.initializationAuth;
+          final user = await repository.currentUser();
 
-      final user = await repository.currentUser();
-      if (user == null) {
-        _redirectToPage(context, LoginScreen());
-      } else {
-        try {
-          repository.clearUserCache();
-          await repository.login(user.username, user.password);
-
-          goToMainScreen(context);
-        } catch (e) {
-          if (JsonUtils.isValidJSONString(e.toString())) {
-            _state = json.decode(e.toString())["msg"];
+          if (user == null) {
+            _redirectToPage(context, LoginScreen());
           } else {
-            setState(() {
-              _state = "Unexpected server error ";
-            });
+            try {
+              repository.clearUserCache();
+              await repository.login(user.username, user.password);
+
+              goToMainScreen(context);
+            } catch (e) {
+              if (JsonUtils.isValidJSONString(e.toString())) {
+                _state = json.decode(e.toString())["msg"];
+              } else {
+                setState(() {
+                  _state = "Unexpected server error ";
+                });
+              }
+            }
           }
         }
-      }
+      });
     });
   }
 
@@ -61,34 +68,23 @@ class _DecisionScreenState extends State<DecisionScreen> {
     );
   }
 
-  Future<void> _redirectToPage(BuildContext context, Widget page) async {
-    final MaterialPageRoute<bool> newRoute =
-        MaterialPageRoute<bool>(builder: (BuildContext context) => page);
-
-    final bool nav = await Navigator.of(context)
-        .pushAndRemoveUntil<bool>(newRoute, ModalRoute.withName('/'));
-    if (nav == true) {
-      //TODO: init
-    }
-  }
-
   void goToMainScreen(BuildContext context) {
-    // TODO goToMainScreen need a separate class we use this method here a and in decision_screen
+    // TODO goToMainScreen need a separate class we use this method here, decision_screen and app_intro_screen
 
     final linkBloc = LinksProvider.of(context);
     final categoriesBloc = CategoriesProvider.of(context);
     final socialBloc = SocialProvider.of(context);
 
-
-    linkBloc.fetchLinks(firstPage,firstPageSize);
+    linkBloc.fetchLinks(firstPage, firstPageSize);
     categoriesBloc.fetchCategories();
 
     _readTime().then((value) {
       if (value.isEmpty) {
-        socialBloc.fetchSocial(page: firstPage , pageSize: firstPageSize);
+        socialBloc.fetchSocial(page: firstPage, pageSize: firstPageSize);
       } else {
         DateTime date = DateTime.parse(value);
-        socialBloc.fetchSocial(date:date,page:firstPage,pageSize: firstPageSize);
+        socialBloc.fetchSocial(
+            date: date, page: firstPage, pageSize: firstPageSize);
       }
     });
 
@@ -100,10 +96,28 @@ class _DecisionScreenState extends State<DecisionScreen> {
     _redirectToPage(context, MainScreen());
   }
 
+  Future<void> _redirectToPage(BuildContext context, Widget page) async {
+    final MaterialPageRoute<bool> newRoute =
+        MaterialPageRoute<bool>(builder: (BuildContext context) => page);
+
+    final bool nav = await Navigator.of(context)
+        .pushAndRemoveUntil<bool>(newRoute, ModalRoute.withName('/'));
+    if (nav == true) {
+      //TODO: init
+    }
+  }
+
   Future<String> _readTime() async {
     final prefs = await SharedPreferences.getInstance();
     final key = 'lastSeen';
     final value = prefs.getString(key) ?? '';
+    return value;
+  }
+
+  Future<bool> _readIntro() async {
+    final prefs = await SharedPreferences.getInstance();
+    final key = 'intro';
+    final value = prefs.getBool(key) ?? false;
     return value;
   }
 
