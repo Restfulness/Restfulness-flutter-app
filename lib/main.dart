87f8ef7821871c6f152b460465b150aa03382cfa
Farzad <farzadi.fm@gmail.com>
2021-01-15 15:15:53 +0330

    Change date picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'constants.dart';
 import 'src/app.dart';
 import 'src/config/app_config.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite/sqflite.dart';
+
 
 void main() {
+  if (Platform.isWindows || Platform.isLinux) {
+    // Initialize FFI
+    sqfliteFfiInit();
+    // Change the default factory
+    databaseFactory = databaseFactoryFfi;
+  }
   AppConfig(
       flavor: Flavor.DEV,
       color: primaryColor,
